56d083469e60663f7ba3f6ddd6f4b3c3f724b16e
Jorge Lopez <jorgelopez.physics@gmail.com>
2020-07-05 17:19:56 +0200

    change css to input placeholder

--- frontend_ecommerce/public/js/custom.js
+++ frontend_ecommerce/public/js/custom.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
             success: function(data) {
                 var btn = $("myBtn");
                 $('#text_in_modal').html(data["date"]) // put date into DOM element inside pop-up window
-                $("#myModal").show('blind', {}, 600) // display pop-up window with effect
+                $("#myModal").show('blind', {}, 600)   // display pop-up window with effect
                 $("#contact").trigger("reset"); // reset the form 
                 setTimeout(function() { // hide the pop-up window after 2 seconds
                     $("#myModal").hide('blind', {}, 100)
